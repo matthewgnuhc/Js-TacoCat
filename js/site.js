83256878f8811ff30f7467ad0f369d37e92c8b50
Matthew Chung <matthewgnuhc@gmail.com>
2021-10-07 07:11:51 -0700

    init commit

--- js/site.js
+++ js/site.js
@@ -1,20 +1,62 @@
+function getUserString() {
+    //Get the user's input
+    let userString = document.getElementById("userString").value;
 
-// Displays a message to the user
-function displayMessage() {
-    // Declare our variable
-    let msg = "";
-    // Pull the value from the input box
-    msg = document.getElementById("message").value;
-    // Alert the user
-    alert(msg);
+    //Reverse the user's input - separate function
+    let revString = reverseString(userString);
+
+    //Compare original input and reversed input - separate function
+    let result = compareStrings(userString, revString);
+
+    //Output Result - separate function
+    displayData(result, userString, revString);
+
+};
+
+function reverseString(userString) {
+    let startValue = userString.length - 1;
+    let endValue = 0;
+
+
+    let revString = "";
+
+    //In a decremented for loop the startValue must be higher than the endValue
+    for (let i = startValue; i >= endValue; i--) {
+        revString += userString[i];
+    };
+
+    return revString;
 }
 
-function displayMessage2() {
-    
-    let msg = "";
-    
-    msg = document.getElementById("message").value;
-    
-    Swal.fire(msg);
+function reverseStringB(userString) {
+    //Tall code using intermediate variables for troubleshooting
+    let splitArray = userString.split("");
+    let revArray = splitArray.reverse();
+    let rejoinArray = revArray.join("");
+
+    //Uses array functions to reverse the string
+    return userString.split('').reverse().join("");
 }
 
+
+function compareStrings(userString, revString) {
+
+    const regEx = /[^A-Z0-9]/ig;
+
+    let normalUser = userString.toLowerCase().replace(regEx, "");
+    let normalRev = revString.toLowerCase().replace(regEx, "");
+
+    
+    if (normalUser == normalRev) {
+        return "was a palindrome";
+    } else {
+        return "was not a palindrome";
+    };
+};
+
+function displayData(result, userString, revString) {
+   
+    let output = document.getElementById("results");
+
+    output.innerHTML = `After reversing your string I found that it ${result}<br />You entered ${userString}<br />Reversed was ${revString}`;
+};
